9986cd6dd85d448b1d747d4587312a3bdf435d8a
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-01-25 22:45:55 +0530

    learning fs module

---
+++ fs.js
@@ -0,0 +1,28 @@
+const fs=require('fs');
+
+
+// fs.writeFileSync("./text.txt", "hello tatti");
+
+// fs.writeFile('./text.txt',"wsdfhsf", (err)=>{
+//   console.log(err);
+// })
+
+// const file=fs.readFileSync('./text.txt', 'utf-8');
+// console.log(file);
+
+// fs.readFile('./text.txt', 'utf-8',(err, data)=>{
+//   if(err){
+//     console.log(err)
+//   }else{
+//     console.log(data)
+//   }
+// } )
+
+// fs.appendFileSync("./text.txt",`${new Date().toString()} \n`);
+
+// fs.copyFileSync('./text.txt', "./copyedfile")
+
+// fs.copyFileSync('./Copy.txt', './Copy3.txt')
+
+// fs.unlinkSync('./Copy.txt')
+
