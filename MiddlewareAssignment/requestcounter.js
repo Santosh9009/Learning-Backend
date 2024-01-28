23d61540d46cfc8541a190f7411197ec8d1deb09
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-01-28 19:35:11 +0530

    Did assignments on middleware

---
+++ MiddlewareAssignment/requestcounter.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const assert = require('assert');
+const express = require('express');
+
+const app = express();
+let requestCount = 0;
+
+// You have been given an express server which has a few endpoints.
+// Your task is to create a global middleware (app.use) which will
+// maintain a count of the number of requests made to the server in the global
+// requestCount variable
+
+function requestcounter(req,res,next){
+  requestCount++;
+  next();
+}
+
+app.get('/user',requestcounter, function(req, res) {
+  res.status(200).json({ name: 'john' });
+});
+
+app.post('/user',requestcounter, function(req, res) {
+  res.status(200).json({ msg: 'created dummy user' });
+});
+
+app.get('/requestCount',requestcounter, function(req, res) {
+  res.status(200).json({ requestCount });
+});
+
+app.listen(3000);
