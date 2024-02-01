622164bedcfd4bc48257f55bd80fcd844586ea6b
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-02-01 23:35:12 +0530

    CourseSellingApp added db schma file

---
+++ CourseSelllingApp/db/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { link } = require('../routes/admin');
+
+// Connect to MongoDB
+mongoose.connect('your-mongodb-url');
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    username : String,
+    password : String,
+});
+
+const UserSchema = new mongoose.Schema({
+    username : String,
+    password : String,
+});
+
+const CourseSchema = new mongoose.Schema({
+    title : String,
+    description : String,
+    price : Number,
+    imageLink : String,
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
