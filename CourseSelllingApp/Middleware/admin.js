56a36ff3363804082da12228e8f6579ce90711c3
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-02-07 23:53:59 +0530

    added middlewares

---
+++ CourseSelllingApp/Middleware/admin.js
@@ -0,0 +1,24 @@
+// Middleware for handling auth
+const { Admin } = require("../db");
+
+function adminMiddleware(req, res, next) {
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  const username = req.headers.username;
+  const password = req.headers.password;
+
+  Admin.findOne({
+    username: username,
+    password: password,
+  }).then((result) => {
+    if (result) {
+      next();
+    } else {
+      res.status(403).json({
+        message: `Admin doesn't exist`,
+      });
+    }
+  });
+}
+
+module.exports = adminMiddleware;
---
+++ CourseSelllingApp/Middleware/user.js
@@ -0,0 +1,21 @@
+const { User } = require("../db");
+async function userMiddleware(req, res, next) {
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const username = req.headers.username;
+  const password = req.headers.password;
+
+  const user = await User.findOne({
+    username: username,
+    password: password,
+  });
+  if (user) {
+    next();
+  } else {
+    res.status(403).json({
+      message: `User doesn't exist`,
+    });
+  }
+}
+
+module.exports = userMiddleware;
